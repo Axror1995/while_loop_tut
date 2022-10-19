a23092ea6511d1d5bdb405e705f45b733278c9ed
ZARIFMAC <uzbzarif@gmail.com>
2022-10-19 17:38:35 +0500

    print

--- bin/while_loop_tut.dart
+++ bin/while_loop_tut.dart
@@ -1,11 +1,9 @@
 void main() {
-  String s = 'code';
+  String s = 'abcdefg2345';
   int i = 0;
-  print(s[i]);
-  i += 1;
-  print(s[i]);
-  i += 1;
-  print(s[i]);
-  i += 1;
-  print(s[i]);
+  while (i < s.length) {
+    print(s[i]);
+    i += 1;
+  }
+  print('end');
 }
